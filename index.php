55e99c36afb250c0121edeee8a3daf5df738fc17
ramonfw <ramonfawork@gmail.com>
2022-07-04 19:42:45 +0200

    Challenge initial code commit

---
+++ index.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use GetWith\CoffeeMachine\Console\MakeDrinkCommand;
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+
+$application->add(new MakeDrinkCommand());
+
+$application->run();
